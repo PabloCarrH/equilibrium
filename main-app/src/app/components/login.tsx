309a4ee5f01dd46e398527c8c159d6bf9021ea02
JeiRoo22 <jeissons-romerop@unilibre.edu.co>
2024-09-11 21:13:47 -0500

    diseño login

--- main-app/src/app/components/login.tsx
+++ main-app/src/app/components/login.tsx
@@ -1,62 +1,78 @@
 import { useState } from 'react';
-import { TextField, Button, Container, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import styles from './login.module.scss';
+import logo from '../../assets/logos/spa-small.png';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+  const [message, setMessage] = useState('');
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
-      });
-
-      if (response.ok) {
-        navigate('/dashboard');
-      } else {
-        // Handle login error
-        alert('Login failed');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+    setMessage(data.message);
   };
 
   return (
-    <Container maxWidth="xs">
-      <Typography variant="h4" gutterBottom>
-        Login
-      </Typography>
-      <TextField
-        label="Email"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <TextField
-        label="Password"
-        type="password"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        fullWidth
-        onClick={handleLogin}
+    <div
+      className="container is-flex is-justify-content-center is-align-items-center"
+      style={{ height: '100vh' }}
+    >
+      <div
+        className={`${styles['border-outline']} is-success card`}
+        style={{ maxWidth: '400px', margin: 'auto' }}
       >
-        Login
-      </Button>
-    </Container>
+        <div className="card-content">
+          <p className="title is-2 is-spaced">
+            Equilibrium Centro de Estética y Spa
+          </p>
+          <div className="is-flex is-justify-content-center">
+            <img src={logo} alt="Logo" />
+          </div>
+          <hr></hr>
+          <p className="subtitle is-4">Autenticación</p>
+          <form onSubmit={handleSubmit}>
+            <div className="field">
+              <label className="label">Nombre de usuario</label>
+              <div className="control">
+                <input
+                  className={`${styles['border-outline']} is-success input`}
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  style={{ color: 'white' }}
+                />
+              </div>
+            </div>
+            <div className="field mb-6">
+              <label className="label">Contraseña</label>
+              <div className="control">
+                <input
+                  className={`${styles['border-outline']} is-success input`}
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  style={{ color: 'white' }}
+                />
+              </div>
+            </div>
+            <div className="field has-addons has-addons-right">
+              <div className="control">
+                <button className="button is-primary is-outlined" type="submit">
+                  Iniciar Sesion
+                </button>
+              </div>
+            </div>
+          </form>
+          {message && <p>{message}</p>}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- main-app/vite.config.ts
+++ main-app/vite.config.ts
@@ -3,21 +3,32 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import path from 'path';
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../node_modules/.vite/main-app',
-
+  resolve: {
+    alias: {
+      'bulma': path.resolve(__dirname, '../node_modules/bulma')
+    }
+  },
   server: {
     port: 4200,
     host: 'localhost',
   },
-
   preview: {
     port: 4300,
     host: 'localhost',
   },
-
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import "../node_modules/bulma/bulma.scss";`,
+  //       includePaths: [path.resolve(__dirname, 'node_modules')]
+  //     }
+  //   }
+  // },
   plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
 
   // Uncomment this if you are using workers.
